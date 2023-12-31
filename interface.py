e6638df78942be95a7dc1891049acaab71f29baa
GreenyPM <patrickmadon130@gmail.com>
2023-12-30 20:41:31 -0500

    1.0.1
    Added Exception Handling

--- interface.py
+++ interface.py
@@ -13,32 +13,36 @@ class Interface:
 
         # This is the while loop continuously running the choices as long as "keepGoing" = True.
         while keepGoing:
-            print("Please input one of the selections below.\n-add game\n-view collection\n-clear\n-export\n-exit")
-            print()
-            userChoice = input()
-
-            if "add game" in userChoice:
-                platformEntry = input("Enter Name of Platform:")
-                gameEntry = input("Enter Name of Game:")
-                print(collectionHandler.addGame(platformEntry,gameEntry))
+            try:
+                print("Please input one of the selections below.\n-add game\n-view collection\n-clear\n-export\n-exit")
                 print()
+                userChoice = input()
+
+                if "add game" in userChoice:
+                    platformEntry = input("Enter Name of Platform:")
+                    gameEntry = input("Enter Name of Game:")
+                    print(collectionHandler.addGame(platformEntry,gameEntry))
+                    print()
     
-            elif "view collection" in userChoice:
-                collectionHandler.showDict()
+                elif "view collection" in userChoice:
+                    collectionHandler.showDict()
+                    print()
+                elif "clear" in userChoice:
+                    print(collectionHandler.clearDict())
+                    print()
+
+                elif "export" in userChoice:
+                    print(collectionHandler.exportDict())
+
+                #This function ends the loop, thus ending the runtime of the program.
+                elif "exit" in userChoice:
+                    keepGoing = False
+
+                else:
+                    print("Invalid Selection Please Try Again")
+                    print()
+            except KeyboardInterrupt:
+                print("You're gonna have to try better than that.")
                 print()
-            elif "clear" in userChoice:
-                print(collectionHandler.clearDict())
-                print()
-
-            elif "export" in userChoice:
-                print(collectionHandler.exportDict())
-
-            #This function ends the loop, thus ending the runtime of the program.
-            elif "exit" in userChoice:
-                keepGoing = False
-
-            else:
-                print("Invalid Selection Please Try Again")
-                print()
-
+                continue
 
