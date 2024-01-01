5e2764dcf31ccd8ed8911e7e0bfce671bff9728b
pjoyner <pjj9927@gmail.com>
2023-12-31 21:23:17 -0500

    cleaner logs. cleaner loop

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ class Interface:
         self.collection_handler = GameDict(self.game_collection)
         self.default_filename = "output.xlsx"
         self.keep_going = True
+        self.display_welcome_message()
 
-    def display_menu(self):
+    def display_welcome_message(self):
         print("\n=== Welcome to the GAME COLLECTOR! ===")
         print("A fine application to track your games")
-        print("Please choose one of the options below:")
+
+    def display_menu(self):
+        print("\nPlease choose one of the options below:")
         print("- add (game)\n- view (collection)\n- import\n- clear\n- export\n- exit")
 
     def get_user_choice(self):
@@ -22,32 +25,24 @@ class Interface:
     def add_game(self):
         platform_entry = input("Enter Name of Platform: ")
         game_entry = input("Enter Name of Game: ")
-        result = self.collection_handler.add_game_entry(platform_entry, game_entry)
-        logger.info(result)
-        print(f"\n{result}\n")
+        self.collection_handler.add_game_entry(platform_entry, game_entry)
 
     def view_collection(self):
         self.collection_handler.display_collection()
 
     def import_collection(self):
         filename = input(f"Enter the filename to import (default: {self.default_filename}): ") or self.default_filename
-        result = self.collection_handler.import_collection(filename)
-        logger.info(result)
-        print(f"\n{result}\n")
+        self.collection_handler.import_collection(filename)
 
     def clear_collection(self):
-        result = self.collection_handler.clear_collection()
-        logger.info(result)
-        print(f"\n{result}\n")
+        self.collection_handler.clear_collection()
 
     def export_collection(self):
         filename = input(f"Enter the filename to export (default: {self.default_filename}): ") or self.default_filename
-        result = self.collection_handler.export_collection(filename)
-        logger.info(result)
-        print(f"\n{result}\n")
+        self.collection_handler.export_collection(filename)
 
     def exit_program(self):
-        logger.info("Exiting the program.")
+        logger.info("Exiting.")
         self.keep_going = False
 
     def process_user_choice(self, user_choice):
@@ -67,11 +62,9 @@ class Interface:
             options.get(user_choice, self.handle_invalid_choice)()
         except KeyboardInterrupt:
             logger.warning("KeyboardInterrupt detected.")
-            print("\nYou're gonna have to try better than that.\n")
 
     def handle_invalid_choice(self):
-        logger.warning("Invalid Selection.")
-        print("Invalid Selection. Please Try Again.")
+        logger.warning("Invalid Selection. Please Try Again.")
 
     def run(self):
         while self.keep_going:
